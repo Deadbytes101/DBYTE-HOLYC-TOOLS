ddf52196ef59827cb51c293b084ed01decdc15a3
DEADBYTE <termay10000@gmail.com>
2026-06-22 20:06:04 +0700

    add package version and checksum files

--- scripts/package-windows.ps1
+++ scripts/package-windows.ps1
@@ -16,4 +16,10 @@ Copy-Item target/release/holytools.exe "$OutDir/holytools.exe"
 Copy-Item README.md "$OutDir/README.md"
 Copy-Item CHANGELOG.md "$OutDir/CHANGELOG.md"
 
+$version = & "$OutDir/holytools.exe" version
+$version | Set-Content -Encoding UTF8 "$OutDir/VERSION.txt"
+
+$hash = Get-FileHash "$OutDir/holytools.exe" -Algorithm SHA256
+"$($hash.Hash.ToLowerInvariant())  holytools.exe" | Set-Content -Encoding UTF8 "$OutDir/SHA256SUMS.txt"
+
 Write-Host "package: $OutDir"
